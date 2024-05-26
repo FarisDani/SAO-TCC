164ee622f7dff66688ca422eaa47fc33c8278cae
FarisDani <farisdani84@gmail.com>
2024-05-27 00:27:16 +0700

    membuat folder node dan php

--- node-app/index.js
+++ node-app/index.js
@@ -7,7 +7,7 @@ const wakafRoutes = require("./wakaf_router");
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
