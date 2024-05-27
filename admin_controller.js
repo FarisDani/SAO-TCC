60e3e61e16a9ff374fc06b4df05b1c2a34307864
FarisDani <126995650+FarisDani@users.noreply.github.com>
2024-05-27 20:04:28 +0700

    Update admin_controller.js

--- admin_controller.js
+++ admin_controller.js
@@ -8,6 +8,25 @@ exports.findAll = function(req, res) {
   });
 };
 
+// Controller method to create a new admin
+exports.create = function(req, res) {
+  const new_admin = new Admin(req.body);
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({
+      error: true,
+      message: "Please provide all required fields",
+    });
+  } else {
+    Admin.create(new_admin, function(err, admin) {
+      if (err) res.send(err);
+      res.json({
+        error: false,
+        message: "Admin added successfully!",
+        data: admin,
+      });
+    });
+  }
+};
 
 // Controller method to get an admin by ID
 exports.findById = function(req, res) {
@@ -17,5 +36,31 @@ exports.findById = function(req, res) {
   });
 };
 
+// Controller method to update an admin by ID
+exports.update = function(req, res) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({
+      error: true,
+      message: "Please provide all required fields",
+    });
+  } else {
+    Admin.update(req.params.id, new Admin(req.body), function(err, admin) {
+      if (err) res.send(err);
+      res.json({
+        error: false,
+        message: "Admin updated successfully!",
+      });
+    });
+  }
+};
 
-
+// Controller method to delete an admin by ID
+exports.delete = function(req, res) {
+  Admin.delete(req.params.id, function(err, admin) {
+    if (err) res.send(err);
+    res.json({
+      error: false,
+      message: "Admin deleted successfully!",
+    });
+  });
+};
