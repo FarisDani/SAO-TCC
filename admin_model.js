f351478dd435912af9c7abaa1ef407d612769aa8
FarisDani <126995650+FarisDani@users.noreply.github.com>
2024-05-27 20:04:58 +0700

    Update admin_model.js

--- admin_model.js
+++ admin_model.js
@@ -6,6 +6,18 @@ var Admin = function(admin) {
   this.password = admin.password;
 };
 
+// Method to create a new admin
+Admin.create = function(newAdmin, result) {
+  dbConn.query("INSERT INTO admin SET ?", newAdmin, function(err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      console.log(res.insertId);
+      result(null, res.insertId);
+    }
+  });
+};
 
 // Method to retrieve all admins
 Admin.findAll = function(result) {
@@ -28,6 +40,34 @@ Admin.findById = function(id, result) {
       result(err, null);
     } else {
       console.log("Admin: ", res);
+      result(null, res);
+    }
+  });
+};
+
+// Method to update an admin by ID
+Admin.update = function(id, admin, result) {
+  dbConn.query(
+    "UPDATE admin SET password=? WHERE id = ?",
+    [admin.password, id],
+    function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
+
+// Method to delete an admin by ID
+Admin.delete = function(id, result) {
+  dbConn.query("DELETE FROM admin WHERE id = ?", [id], function(err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
       result(null, res);
     }
   });
