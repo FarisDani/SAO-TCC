4008cb401cbec35bd2ef79a6ef7d62f0c55e911d
FarisDani <126995650+FarisDani@users.noreply.github.com>
2024-05-27 20:08:40 +0700

    Update wakaf_controller.js

--- wakaf_controller.js
+++ wakaf_controller.js
@@ -33,6 +33,22 @@ exports.findById = function(req, res) {
   });
 };
 
+exports.update = function(req, res) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({
+      error: true,
+      message: "Please provide all required fields",
+    });
+  } else {
+    Wakaf.update(req.params.id, new Wakaf(req.body), function(err, wakaf) {
+      if (err) res.send(err);
+      res.json({
+        error: false,
+        message: "Wakaf updated successfully!",
+      });
+    });
+  }
+};
 
 exports.delete = function(req, res) {
   Wakaf.delete(req.params.id, function(err, wakaf) {
