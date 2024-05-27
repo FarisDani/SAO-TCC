07f5e994b31968855fa48a1914674090d143957d
FarisDani <126995650+FarisDani@users.noreply.github.com>
2024-05-27 20:05:27 +0700

    Update admin_router.js

--- admin_router.js
+++ admin_router.js
@@ -4,7 +4,17 @@ const controller = require("./admin_controller");
 
 // Route to get all admins
 router.get("/", controller.findAll);
+
+// Route to create a new admin
+router.post("/", controller.create);
+
 // Route to get an admin by ID
 router.get("/:id", controller.findById);
+
+// Route to update an admin by ID
+router.put("/:id", controller.update);
+
+// Route to delete an admin by ID
+router.delete("/:id", controller.delete);
 
 module.exports = router;
